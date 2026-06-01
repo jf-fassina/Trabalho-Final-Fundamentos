1e9b2a33f1685b9f0ee729fad9ebf73921339dcb
João Francisco Scoloski Fassina <jf.fassina@gmail.com>
2026-06-01 10:16:11 -0300

    initial commit

---
+++ src/Application.java
@@ -0,0 +1,21 @@
+public class Application {
+
+    /*
+    Crie uma classe Application que deverá ter o método main com as seguintes funcionalidades que
+    deverão acionar o(s) métodos adequados para a realização da operação escolhida.
+    1 – Adicionar prato no cardápio
+    2 – Remover prato do cardápio
+    3 – Consultar cardápio
+    4 – Adicionar ingrediente no prato
+    5 – Remover ingrediente do prato
+    6 – Prato mais econômico
+    7 – Pratos que contém um determinado ingrediente
+    8 – VOCÊ DEFINE E IMPLEMENTA ESTA FUNCIONALIDADE
+    10 – Sair do programa
+     */
+
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/Ingrediente.java
@@ -0,0 +1,35 @@
+public class Ingrediente {
+    private String nome, medida;
+    private double quantidade;
+
+
+    public Ingrediente(String nome, String medida, double quantidade) {
+        this.nome = nome;
+        this.medida = medida;
+        this.quantidade = quantidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMedida() {
+        return medida;
+    }
+
+    public void setMedida(String medida) {
+        this.medida = medida;
+    }
+
+    public double getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(double quantidade) {
+        this.quantidade = quantidade;
+    }
+}
